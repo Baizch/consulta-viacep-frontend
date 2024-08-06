d811026ca052756b2f0460c9152fa714d66556c5
baizch <francobaisch00@gmail.com>
2024-08-06 11:18:30 -0300

    added missing /cep

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -23,7 +23,7 @@ const handleSearch = async (): Promise<void> => {
     try {
         // const response = await axios.get(`${apiUrl}/${inputValue.value}`)
 
-        const response = await axios.get(`/api/${inputValue.value}`)
+        const response = await axios.get(`/api/cep/${inputValue.value}`)
 
         if (response.data.erro) {
             address.value = { error: 'CEP inválido' } as Address
